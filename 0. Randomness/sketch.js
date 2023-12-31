cfac7831f9b3f070c7c5a488df55cd2076cde878
Kiryl Shmarlouski <kiryl.shmarlouski@ext.fmc-ag.com>
2023-12-31 13:03:34 +0200

    add random gaussian

--- 0. Randomness/sketch.js
+++ 0. Randomness/sketch.js
@@ -1,26 +1,86 @@
-function setup() {
-    createCanvas(720, 560);
-    noStroke();
-    noLoop();
+
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
   }
-  
-  function draw() {
-    drawCircle(width / 2, 280, 6);
+
+  draw() {
+    stroke(0);
+    strokeWeight(5);
+    point(this.x, this.y);
   }
-  
-  function drawCircle(x, radius, level) {
-    // 'level' is the variable that terminates the recursion once it reaches 
-    // a certain value (here, 1). If a terminating condition is not 
-    // specified, a recursive function keeps calling itself again and again
-    // until it runs out of stack space - not a favourable outcome! 
-    const tt = (126 * level) / 4.0;
-    fill(tt);
-    ellipse(x, height / 2, radius * 2, radius * 2);
-    if (level > 1) {  
-      // 'level' decreases by 1 at every step and thus makes the terminating condition
-      // attainable
-      level = level - 1;  
-      drawCircle(x - radius / 2, radius / 2, level);
-      drawCircle(x + radius / 2, radius / 2, level);
+
+  step() {
+    let step = 1;
+    let r = random(1);
+
+    if (r < 0.5) {
+      if (this.x < mouseX) {
+        this.x += step;
+      } else {
+        this.x -= step;
+      }
+      if (this.y < mouseY) {
+        this.y += step;
+      } else {
+        this.y -= step;
+      }
+
+    } else {
+
+      if (r < 0.6) {
+        this.x += step;
+      } else if (r < 0.7) {
+        this.x -= step;
+      } else if (r < 0.8) {
+        this.y += step;
+      } else {
+        this.y -= step;
+      }
     }
+
   }
+}
+
+let walker;
+
+
+// default walker
+// function setup() {
+//   createCanvas(windowWidth, 800);
+//   walker = new Walker();
+//   background(255);
+
+// }
+// function draw() {
+//   walker.step();
+//   walker.draw();
+// }
+
+
+// random gaussian dots
+function setup() {
+  createCanvas(windowWidth, 800);
+  background(255);
+
+  slider = createSlider(2, 100, 50);
+  slider.position(10, 10);
+  slider.size(150);
+}
+
+function draw() {
+  let d = slider.value();
+  let x = randomGaussian(windowWidth / 2, d);
+  let y = randomGaussian(windowHeight / 2, d);
+
+  // gaussian color
+  let r = randomGaussian(125, 80);
+  let g = randomGaussian(125, 80);
+  let b = randomGaussian(125, 80);
+
+  stroke(r, g, b);
+  strokeWeight(5);
+  point(x, y);
+
+}
