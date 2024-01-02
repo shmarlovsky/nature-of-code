4a7ee1d4e865ffb96202a179adaafb9623f0dd89
Kiryl Shmarlouski <kiryl.shmarlouski@ext.fmc-ag.com>
2024-01-02 16:24:08 +0200

    add follow mouse for bouncing ball

--- 1. Vectors/ball.js
+++ 1. Vectors/ball.js
@@ -12,13 +12,14 @@ class Mover {
   constructor(radius) {
     this.position = createVector(random(radius, width - radius), random(radius, height - radius));
     this.velocity = createVector(random(2, 4), random(2, 4));
-    this.velocity.limit(10);
-    // this.acceleration = createVector(0.005, 0.005);
-    this.acceleration = p5.Vector.random2D();
+    this.acceleration = createVector(0.005, 0.005);
+    // this.acceleration = p5.Vector.random2D();
     this.radius = radius;
     this.nOff = 1000;
+    this.topSpeed = 10;
   }
 
+  // bouncing ball with random noise acceleration
   update() {
     this.acceleration = p5.Vector.random2D();
     // this.acceleration.mult(random(0.7));
@@ -45,6 +46,18 @@ class Mover {
     }
   }
 
+  updateFollowMouse() {
+    let mouse = createVector(mouseX, mouseY);
+    let dir = p5.Vector.sub(mouse, this.position);
+    dir.normalize();
+    dir.mult(0.2);
+
+    this.acceleration = dir;
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.topSpeed);
+    this.position.add(this.velocity);
+  }
+
   draw() {
     stroke(0, 0, 200);
     fill(0, 0, 200);
@@ -63,7 +76,8 @@ function setup() {
 
 function draw() {
   background(255);
-  m.update();
+  // m.update();
+  m.updateFollowMouse();
   m.draw();
 }
 
